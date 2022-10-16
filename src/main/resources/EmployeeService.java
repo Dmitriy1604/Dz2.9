2ade959cc5c9c9b3398110c782f13d13b453411d
Dmitriy1604 <Dimon312-91@mail.ru>
2022-10-16 12:31:28 +0300

    Выполнил

---
+++ src/main/resources/EmployeeService.java
@@ -0,0 +1,16 @@
+package service;
+
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstName, String lastName, int salary, int department);
+    Employee removeEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
+    Employee getLowestPaidEmployee(int department);
+    Employee getHighestPaidEmployee(int department);
+    List<Employee> printEmployeesForDepartment(int department);
+    List<Employee> printEmployeesByDepartments();
+    List<Employee> printEmployees();
+    List<Employee> fillEmployeesList();
+}
